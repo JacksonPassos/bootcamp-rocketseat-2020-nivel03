9dfcc439cb52394d235f78fc7c68f78abdacf249
Jackson Passos <jacksonguitar33@gmail.com>
2021-01-24 22:07:44 -0300

    added routes config

--- reactjs-github-explorer/src/App.tsx
+++ reactjs-github-explorer/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
-}
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes'
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+)
 
 export default App;
