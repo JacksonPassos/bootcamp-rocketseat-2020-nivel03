3a2ddb3c7ea6bec95d1706fb3dfd7f67e718b811
Jackson Passos <jacksonguitar33@gmail.com>
2021-01-24 23:02:56 -0300

    initial config styles globals

--- reactjs-github-explorer/src/App.tsx
+++ reactjs-github-explorer/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom'
 
+import GlobalStyles from './styles/global'
+
 import Routes from './routes'
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
 )
 
 export default App;
